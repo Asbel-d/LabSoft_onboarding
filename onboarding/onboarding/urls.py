9f1b253bc58c40a85737ca2e6c4f8b79312b85f3
Asbel Duque <asbel.duque@utp.edu.co>
2026-04-19 20:58:56 -0500

    Add root route for Render

--- onboarding/onboarding/urls.py
+++ onboarding/onboarding/urls.py
@@ -28,6 +28,7 @@ from accounts.views import (
 )
 
 urlpatterns = [
+    path('', redireccion_por_rol, name='home'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
